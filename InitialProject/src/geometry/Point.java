5af4e97edf8808064ae5534ea84f1d5f3a7b9ff0
DejanaGladic <dejanagladic@uns.ac.rs>
2026-03-30 08:58:14 +0200

    Druge vezbe - prvi deo

--- InitialProject/src/geometry/Point.java
+++ InitialProject/src/geometry/Point.java
@@ -5,6 +5,13 @@ public class Point {
 	private int x;
 	private int y;
 	private boolean selected;
+	
+	public double distance(Point point2) {
+		double a = this.x - point2.x;
+		double b = this.y - point2.y;
+		double c = Math.sqrt(a*a+b*b);
+		return c;
+	}
 
 	public int getX() {
 		return x;
--- InitialProject/src/geometry/Test.java
+++ InitialProject/src/geometry/Test.java
@@ -7,10 +7,51 @@ public class Test {
 		p.setX(10);
 		p.setY(20);
 		p.setSelected(true);
-		
+
 		System.out.println("Koordinata x: " + p.getX());
 		System.out.println("Koordinata y: " + p.getY());
 		System.out.println("Selected: " + p.isSelected());
+
+		Point point2 = new Point();
+		point2.setX(50);
+		point2.setY(40);
+		double distance = p.distance(point2);
+		System.out.println("Distanca je: " + distance);
+
+		/* Vezbe 2 - zadacici */
+
+		// 1. Inicijalizovati x koordinatu tacke p
+		// na vrednost y koordinate tacke p1
+
+
+		// 2. Postaviti za pocetnu tacku linije l1 tacku p, a
+		// za krajnju tacku linije l1 tacku p1
+
+
+
+		// 3. Postaviti y koordinatu krajnje tacke l1 na 23
+
+
+		// 4. Inicijalizovati x koordinatu pocetne tacke linije l1
+		// na vrednost y koordinate krajnje tacke linije l1
+
+
+		// 5. Postaviti x koordinatu krajnje tacke l1 na vrednost
+		// duzine linije l1 umanjene za vrednost zbira x i y
+		// koordinate pocetne tacke linije l1
+
+
+		// 6. Postaviti x koordinatu tacke gore levo pravougaonika
+		// r1 na vrednost 10 i y koordinatu na vrednost 15
+
+
+		// 7. Postaviti centar kruga c1 na koordinate tacke
+		// gore levo od r1
+
+
+		// 8. Postaviti x koordinatu centra kruga c1 na vrednost razlike
+		// povrsine pravougaonika r1 i y koordinate pocetne tacke linije l1
+
 
 	}
 
